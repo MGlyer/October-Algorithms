2470b31b68f154d8900ce6383de00152c341c928
Martin <MartinGlyer@gmail.com>
2018-10-09 18:20:43 -0400

    solves a semi efficient Largest Sub Array

---
+++ LargestSubArray.js
@@ -0,0 +1,27 @@
+const findLargetSubArray = (array) => {
+    let longestArray = []
+
+    const arrayChecker = (arr) => {
+        let numCount = 0
+        let strCount = 0
+        arr.forEach((item) => {
+            if (typeof item === 'string') strCount++
+            if (typeof item === 'number') numCount++
+        })
+        return numCount === strCount
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (longestArray.length > (array.length - i)) break
+        let currentArr = [array[i]]
+        let evenFlag = true
+        for (let j = i+1; j < array.length; j++) {
+            currentArr.push(array[j])
+            if (evenFlag && arrayChecker(currentArr)) {
+                if (currentArr.length > longestArray.length) longestArray = currentArr.slice()
+            }
+            evenFlag = !evenFlag
+        }
+    }
+    return longestArray
+}
