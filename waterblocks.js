403f17f9b24e0225abd6d15a92b1d470bd61c161
Martin <MartinGlyer@gmail.com>
2018-10-11 09:22:50 -0400

    solves waterblocks in linear time

--- waterblocks.js
+++ waterblocks.js
@@ -2,8 +2,73 @@ const waterblocks = (arr) => {
   //left pass, right pass
   //calculate water from each pass, but only where the stopping wall
   //is greater than or equal to the height of the starting wall on each pass.
+  
+    let hist = {}
+    let wall = false
+    let wallIndex;
+    let wallHeight;
+    let waterCount = 0
+    let currentWater = 0
 
-  //declare evenHeightHist = {}
+    //left pass
+
+    for (let i = 0; i < arr.length; i++) {
+      if (wall) {
+        if (wallHeight > arr[i]) {
+          currentWater += (wallHeight - arr[i])
+          continue
+        } else {
+          //finds the ending wall, dump currnetWater count into total water
+          wall = false
+          waterCount += currentWater
+          currentWater = 0
+          //this prevents double counting, for right pass to skip
+          if (wallHeight === arr[i]) {
+            hist[i] = wallIndex
+          }
+        }
+      }
+
+      //check if we need to start counting water again
+      if (!wall && arr[i] > arr[i+1]) {
+        wall = true
+        wallHeight = arr[i]
+        wallIndex = i
+      } 
+    }
+
+    //rightpass, reset items
+    wall = false
+    currentWater = 0
+
+    for (let i = arr.length-1; i >= 0; i--) {
+      //check if we need to skip a section of water. avoids double counting
+      if (hist.hasOwnProperty(i)) {
+        i = hist[i] + 1
+      }
+
+      if (wall) {
+        if (wallHeight > arr[i]) {
+          currentWater += (wallHeight - arr[i])
+          continue
+        } else {
+          wall = false
+          waterCount += currentWater
+          currentWater = 0
+        }
+      }
+
+      if (!wall && arr[i] > arr[i-1]) {
+        wall = true
+        wallHeight = arr[i]
+      }
+    }
+    return waterCount
+
+    
+}
+
+//declare evenHeightHist = {}
   //declare wall = false
   //declare wallHeight = -1
   //declare waterCount = 0
@@ -21,55 +86,3 @@ const waterblocks = (arr) => {
     //if not wall
       //compare current height to next height
         //if heigher, wall = true
-
-    let hist = {}
-    let wall = false
-    let wallIndex = -1
-    let wallHeight = -1
-    let waterCount = 0
-
-    //left pass
-
-    for (let i = 0; i < arr.length; i++) {
-      if (wall) {
-        if (wallHeight > arr[i]) {
-          waterCount+= (wallHeight - arr[i])
-          continue
-        } else {
-          wall = false
-          if (wallHeight === arr[i]) {
-            hist[i] = wallIndex
-          }
-        }
-      }
-
-      if (!wall && arr[i] > arr[i+1]) {
-        wall = true
-        wallHeight = arr[i]
-        wallIndex = i
-      } 
-    }
-
-    //rightpass
-
-    for (let i = arr.length-1; i > 0; i--) {
-      if (hist.hasOwnProperty(i)) {
-        i = hist[i] + 1
-      }
-
-      if (wall) {
-        if (wallHeight > arr[i]) {
-          waterCount+= (wallHeight - arr[i])
-            continue
-        } else {
-          wall - false
-        }
-      }
-
-      if (!wall && arr[i] > arr[i-1]) {
-        wall = true
-        wallHeight = arr[i]
-      }
-    }
-    
-}
