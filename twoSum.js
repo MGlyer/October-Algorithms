ede72a7a2125d86318c4eb02285d88dd72a4486d
Martin <MartinGlyer@gmail.com>
2018-10-12 14:54:17 -0400

    solves twoSum in Linear

--- twoSum.js
+++ twoSum.js
@@ -1,7 +1,7 @@
 const twoSum = (array, target) => {
   let hist = {}
   let answer;
-  array.forEach(el, i => {
+  array.forEach((el, i) => {
     let pair = target - el
     if (hist.hasOwnProperty(pair)) answer = [hist[pair], i]
     else hist[el] = i
