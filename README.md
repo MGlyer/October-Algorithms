795dc9adc7eae2323ebf47956f2cc0f219e0c762
Martin <MartinGlyer@gmail.com>
2018-10-12 15:09:49 -0400

    moves MajorityElement pseudocode to end of file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # October-Algorithms
+
+contains all the work I do on toy problems in the month of October, 2018
--- majorityElement.js
+++ majorityElement.js
@@ -1,16 +1,6 @@
 const majorityElement = (array) => {
-  //let count equal 1
   let count = 1
-  //assume first item of array is candidate
   let candidate = array[0]
-  //loop thru array, start at 2nd index
-  //if item is equal to candidate
-    //count++
-  //else
-    //count --
-    //if count = 0
-      //candidate = item at that index
-      //count = 1
   for (let i = 1; i < array.length; i++) {
     let el = array[i];
     if (el === candidate) count ++
@@ -22,17 +12,12 @@ const majorityElement = (array) => {
       }
     }
   }
-  //count = 0
   count = 0
   for (let i = 0; i < array.length; i++) {
     let el = array[i];
     if (el === candidate) count ++
   }
-  //loop thru again
-    //if item = candidate
-      //count ++
   return count >= array.length/2 ? candidate : 'no majority element'
-  //return count >= length of array/2 (is majority)
 }
 
 let test1 = [2, 3, 2, 2, 5, 6, 2, 7, 3, 2, 2, 1]
@@ -40,3 +25,19 @@ let test2 = [1, 2, 2, 3, 4]
 
 console.log(majorityElement(test1));
 console.log(majorityElement(test2));
+
+//let count equal 1
+//assume first item of array is candidate
+//loop thru array, start at 2nd index
+  //if item is equal to candidate
+    //count++
+  //else
+    //count --
+    //if count = 0
+      //candidate = item at that index
+      //count = 1
+//count = 0
+//loop thru again
+//if item = candidate
+//count ++
+//return count >= length of array/2 (is majority)
