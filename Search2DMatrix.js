611f3571a712d745a856c1fa5683c112d8273b63
Martin <MartinGlyer@gmail.com>
2018-10-15 10:02:32 -0400

    solves 2d matrix in 2 O(n) time

--- Search2DMatrix.js
+++ Search2DMatrix.js
@@ -3,8 +3,11 @@ The first integer of each row is greater than the last integer of the previous r
 */
 
 //there is probably also a log way of doing this
+//current time: 2N
 
 const search = (matrix, target) => {
+  if (matrix.length < 1) return false
+
   let arrToSearch = -1
   for (var i = 0; i < matrix.length; i ++) {
     if (target < matrix[i][0]) {
@@ -14,5 +17,20 @@ const search = (matrix, target) => {
   }
 
   if (arrToSearch < 0) arrToSearch = matrix.length-1
-  return matrix[arrToSearch].contains(target)
+  return matrix[arrToSearch].includes(target)
 }
+
+// let matrix1= [
+//   [1,   3,  5,  7],
+//   [10, 11, 16, 20],
+//   [23, 30, 34, 50]
+// ]
+
+// let matrix2 = [
+//   [1,   3,  5,  7],
+//   [10, 11, 16, 20],
+//   [23, 30, 34, 50]
+// ]
+
+// console.log(search(matrix1, 34))
+// console.log(search(matrix1, 60))
