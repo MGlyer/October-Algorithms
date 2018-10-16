d5b281c443e726e40196b2bf64d7685c65a884c0
Martin <MartinGlyer@gmail.com>
2018-10-16 16:28:02 -0400

    first stab at SearchMatrix2 code

--- SearchMatrix2.js
+++ SearchMatrix2.js
@@ -19,11 +19,66 @@ Consider the following matrix:
 const searchMatrix2 = (matix, target) => {
   //row ind arr
   //col ind arr
+  let rowInd = [0, matrix.length-1]
+  let colInd = [0, matrix[0].length-1]
   //iterate thru matrix
     //check if target within row
       //if so add ind to row arr
     //check if target within col
       //if so add ind to col arr
+
+  let flag = false
+  const recurse = (matrix) => {
+    let rowStart = false
+    for (var i = rowInd[0]; i <= rowInd[1]; i++) {
+      if (target > matrix[i][colInd[0]] && target < matrix[i][colInd[1]]) {
+        if (!rowStart) {
+          rowStart = true
+          rowInd[0] = i
+        }
+      }
+      if (target < matrix[i][colInd[0]] || target > matrix[i][colInd[1]]) {
+        if (rowStart) {
+          rowInd[1] = i
+          break
+        }
+      }
+    }
+
+
+    let colStart = false
+    for (var i = colInd[0]; i <= colInd[1]; i++) {
+      if (target > matrix[rowInd[0]][i] && target < matrix[rowInd[1]][i]) {
+        if (!colStart) {
+          colStart = true
+          colInd[0] = i
+        }
+      }
+      if (target < matrix[rowInd[0]][i] || target > matrix[rowInd[1]][i]) {
+        if (rowStart) {
+          colInd[1] = i
+          break
+        }
+      }
+    }
+
+    if (!rowStart || !colStart) {
+      flag = false
+      return
+      // return false
+    }
+
+    if (rowInd[1] === rowInd[0] && colInd[1] === colInd[0]) {
+      flag = true
+      return
+      // return true
+    }
+
+    recurse(matrix)
+  }
+
+  recurse(matrix)
+  return flag
   
   //declare flag false
   //declare recurse alg, which takes a matrix
