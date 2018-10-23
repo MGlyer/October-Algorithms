d975c104236e9e0755ead2cd01d8fe73012eee6b
Martin <MartinGlyer@gmail.com>
2018-10-23 14:30:48 -0400

    new idea for how to solve LPRP

--- theSkylineProblem.js
+++ theSkylineProblem.js
@@ -7,6 +7,33 @@ collectively (Figure B).
 */
 
 const skyline = (array) => {
+  //left pass, right pass method
+  //high level: left pass would push tuples of the top
+  //rigth pass would push tuples of the low part of the jump
+  //sort by left index
+  let city = [];
+
+  let currentHeight = 0;
+  let blockEnd = -1
+  for (var i = 0; i < array.length; i++) {
+    let building = array[i]
+    
+    if (building[2] > currentHeight || building[0] > blockEnd) {
+      curentHeight = building[2]
+      city.push([building[0], currentHeight])
+      continue
+    }
+
+    //tracks when the height should go back to 0
+    if (building[1] > blockEnd) {
+      blockEnd = building[1]
+    }
+  }
+
+
+
+
+  //THE BELOW IS A LIE.  DO THE ABOVE INSTEAD
   //sort array by index 0 of triplet (start of building)
   //declar answer arr
   //declare current height
